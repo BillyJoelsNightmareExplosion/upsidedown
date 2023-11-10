594064fd427696a6df9139dded58553d76b77f89
downloadjpg <brody.foster@gmail.com>
2023-11-10 18:00:04 -0500

    huh

--- the_big_circle.gd
+++ the_big_circle.gd
@@ -3,10 +3,11 @@ extends Path3D
 
 var t = 12
 @export_range(0, 1, 0.01) var wheee = 0.1
+@onready var follow_nodes = get_children()
 
 func _process(delta):
-	var follow_nodes = get_children()
-	for i in range(0, follow_nodes.size()):
+	if wheee > 0:
+		for i in range(0, follow_nodes.size()):
 		#follow_nodes[i].progress_ratio = (float(i) / follow_nodes.size()) + time
-		follow_nodes[i].progress_ratio += delta * wheee
+			follow_nodes[i].progress_ratio += delta * wheee
 		

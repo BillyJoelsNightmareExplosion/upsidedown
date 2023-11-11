6c2afae4001bebf6f3a1813a406635b50bb68bc8
downloadjpg <brody.foster@gmail.com>
2023-11-11 13:14:24 -0500

    fixed mixed tab/spaces

--- main.gd
+++ main.gd
@@ -3,16 +3,16 @@ extends Node3D
 
 
 func _input(event):
-    if event is InputEventKey:
-          if event.pressed and event.keycode == KEY_ESCAPE:
-             get_tree().quit()
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_ESCAPE:
+			get_tree().quit()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-    pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-    pass
+	pass

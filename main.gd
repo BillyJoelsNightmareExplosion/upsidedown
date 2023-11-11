0a219fd2ddb863c1727737fddba5259a3c503227
Alec Tremblay <tremblayalec14@gmail.com>
2023-11-10 23:30:15 -0500

    adds main, player, level, wall

---
+++ main.gd
@@ -0,0 +1,18 @@
+extends Node3D
+
+
+
+func _input(event):
+    if event is InputEventKey:
+          if event.pressed and event.keycode == KEY_ESCAPE:
+             get_tree().quit()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+    pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+    pass

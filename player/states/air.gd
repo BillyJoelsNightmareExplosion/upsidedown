93f0a867851e6c8e0bc66a3d8adccdc8fe75849a
downloadjpg <brody.foster@gmail.com>
2023-11-12 14:10:09 -0500

    wall running! make sure wall is in layer 3 for this to work

--- player/states/air.gd
+++ player/states/air.gd
@@ -13,20 +13,16 @@ func enter(msg := {}):
 func physics_update(delta):
 	
 	var vec = calculate_xz_vector(Vector2(player.velocity.x, player.velocity.z), Vector2(player.direction.x, player.direction.z), delta)
+	var transitioned: bool
+	transitioned = landing_check()
+	if transitioned:
+		return
 	
-	# Landing check
-	if player.is_on_floor():
-		#player.anim_tree.active = true
-		if player.velocity.is_equal_approx(Vector3.ZERO):
-			state_machine.transition_to("Idle")
-			return
-		elif is_equal_approx(player.velocity.y, 0): # this one caused an infinite loop :D 
-			if Input.is_action_pressed("move_spr"):
-				state_machine.transition_to("Sprint")
-				return
-			else:
-				state_machine.transition_to("Walk")
-				return
+	# Wall run check
+	transitioned = wall_run_check()
+	if transitioned:
+		return
+	
 	player.velocity.x = vec.x
 	player.velocity.z = vec.y
 	player.apply_gravity(delta)
@@ -34,6 +30,49 @@ func physics_update(delta):
 
 	
 
+func landing_check() -> bool:
+	# Landing check
+	if player.is_on_floor():
+		#player.anim_tree.active = true
+		if player.velocity.is_equal_approx(Vector3.ZERO):
+			state_machine.transition_to("Idle")
+			return true
+		elif is_equal_approx(player.velocity.y, 0): # this one caused an infinite loop :D 
+			if Input.is_action_pressed("move_spr"):
+				state_machine.transition_to("Sprint")
+				return true
+			else:
+				state_machine.transition_to("Walk")
+				return true
+	return false
+
+func wall_run_check() -> bool:
+	if not was_sprinting:
+		return false
+	if Input.is_action_just_pressed("move_jum"): # infinite loop catch
+		return false
+	#if player.velocity.y:
+	#	return false
+
+	var left_collide = player.wall_left_cast.is_colliding()
+	var right_collide = player.wall_right_cast.is_colliding()
+	var hit : RayCast3D
+	var side : String
+	if left_collide:
+		side = "left"
+		hit = player.wall_left_cast
+	if right_collide:
+		side = "right"
+		hit = player.wall_right_cast
+
+	if (left_collide or right_collide) and was_sprinting:
+		state_machine.transition_to("WallRun", {
+			wall_side = side,
+			entrance_direction = player.velocity,
+			wall_normal = hit.get_collision_normal()
+		})
+		return true
+	return false
 
 
 # This function calculates an xz_vector as air movement is a lot more complicated than ground.

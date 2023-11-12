8f5b8fa4a4f3a911f5ca07c16addc763a6e75def
eli-johnson-04 <elijah.johnson@ufl.edu>
2023-11-11 19:25:08 -0500

    collectable place proof of concept
    allows for the placement of spheres in the world, next i'm working on picking them up

--- game_manager.gd
+++ game_manager.gd
@@ -9,10 +9,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("place_pickup"):
+		place(get_tree().get_root().get_node("main/player").global_position)
 
 # Takes in position of the player, places the caller at that position
 func place(position : Vector3):
-	var placedCollectable = obj.instance()
+	var placedCollectable = obj.instantiate()
 	get_tree().get_root().get_node("main").add_child(placedCollectable)
 	placedCollectable.global_position = position

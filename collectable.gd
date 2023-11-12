952c7989e89811d24ed36c14e90caeaad5c0147d
eli-johnson-04 <elijah.johnson@ufl.edu>
2023-11-11 21:29:04 -0500

    nearly complete mesh show/hide system
    allows for a specified number of collectibles to be placed, currently set to 8
    
    has a system for changing the mesh to designate a different trinket, which should function like a stack in that from 0-max_children, the lowest value should be shown and the rest get set to false. currently does not work correctly

--- collectable.gd
+++ collectable.gd
@@ -2,10 +2,22 @@ extends Node3D
 
 var pickuppable = false
 
+@onready var models = [
+	$Area3D/MeshInstance3D,
+	$Area3D/box,
+	$Area3D/capsule,
+	$Area3D/cylinder,
+	$Area3D/plane,
+	$Area3D/quad,
+	$Area3D/prism,
+	$Area3D/torus
+]
+
+# When the player touches the collectable, delete it and subtract the total number of collectables
 func _on_area_3d_body_entered(body):
-	print("balls")
 	if pickuppable:
 		queue_free()
+		GameManager.numPlaced -= 1
 
 func is_player_touching_me() -> bool:
 	return $Area3D.get_overlapping_bodies().size() != 0
--- game_manager.gd
+++ game_manager.gd
@@ -1,17 +1,36 @@
 extends Node
 
+var MAX_COLLECTABLES = 8
+
 var obj = preload("res://collectable.tscn")
 
+var numPlaced = 0
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+
+
+
+# if the player is placing a collectable, place it at the player's position and update the count
 func _process(delta):
 	if Input.is_action_just_pressed("place_pickup"):
-		place(get_tree().get_root().get_node("main/player").global_position)
+		if numPlaced < MAX_COLLECTABLES:
+			place(get_tree().get_root().get_node("main/player").global_position)
 
-# Takes in position of the player, places the caller at that position
+# Takes in position of the player, places the collectable at that position
 func place(position : Vector3):
+	numPlaced += 1
 	var placedCollectable = obj.instantiate()
 	get_tree().get_root().get_node("main").add_child(placedCollectable)
 	placedCollectable.global_position = position
+	
+	# todo: fix so that it shows the lowest index of the models array and hides the rest
+	#var min
+	#for i in range(0, 8):
+		#if placedCollectable.models[i].visible == false:
+		#	placedCollectable.models[i].visible = true
+		#else:
+		#	placedCollectable.models[i].visible = false
+	for i in range(0, 8):
+		placedCollectable.models[i].visible = (i == numPlaced - 1)
+	
 	await placedCollectable.get_node("Area3D").body_exited
 	placedCollectable.pickuppable = true
